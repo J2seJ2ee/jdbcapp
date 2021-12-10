d2b21d21353953023a38a09efdd37880608fa1a7
Pawan <j2seplusj2ee>
2021-12-10 19:47:31 +0530

    complete code crud

--- src/jdbcapp/JDBCApp.java
+++ src/jdbcapp/JDBCApp.java
@@ -1,21 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jdbcapp;
 
-/**
- *
- * @author root
- */
-public class JDBCApp {
+import java.util.ArrayList;
+import java.util.Scanner;
+import jdbcapp.dao.ProductDAO;
+import jdbcapp.model.Product;
 
-    /**
-     * @param args the command line arguments
-     */
+public class JDBCApp {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Product p = null;
+        int x = 0;
+        do{
+            System.out.println("Press 1 for insert");
+            System.out.println("Press 2 for delete");
+            System.out.println("Press 3 for update");
+            System.out.println("Press 4 for select");
+            System.out.println("Press 5 for exit");
+            System.out.println("Enter Your choice");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1: 
+                    System.out.println("Enter Product Name");
+                    String pname = sc.next();
+                    System.out.println("Enter Product Price");
+                    float price = sc.nextFloat();
+                    System.out.println("Enter Description");
+                    String desc = sc.next();
+                     p = new Product(pname, price, desc);
+                    x = new ProductDAO().insert(p);
+                    if(x!=0)
+                        System.out.println("Record Inserted...");
+                    else
+                        System.out.println("Record Not Inserted...");
+                    break;
+                case 2: 
+                    System.out.println("Enter Product Id to be deleted...");
+                    int pid = sc.nextInt();
+                    p = new Product();
+                    p.setId(pid);
+                    x = new ProductDAO().delete(p);
+                    if(x!=0)
+                        System.out.println("Record Deleted..");
+                    else
+                        System.out.println("Record Not Deleted...");
+                    break;
+                case 3: 
+                    System.out.println("Enter product id to be updated...");
+                    int productId = sc.nextInt();
+                    System.out.println("Enter Product Name");
+                    String productName = sc.next();
+                    System.out.println("Enter Price");
+                    float productPrice = sc.nextFloat();
+                    System.out.println("Enter Desription");
+                    String productDesc = sc.next();
+                    
+                    p = new Product(productId, productName, productPrice, productDesc);
+                    x = new ProductDAO().update(p);
+                    if(x!=0)
+                        System.out.println("Record Updated...");
+                    else
+                        System.out.println("Record Not Updated...");
+                    break;
+                case 4: 
+                    ArrayList<Product> al = new ProductDAO().getProductList();
+                    for(Product product: al){
+                        System.out.println(product.getId()+" "+product.getName()+" "+product.getPrice()+" "+product.getDescription());
+                    }
+                    break;    
+                case 5: 
+                    System.exit(0);
+                    break;
+            }
+        }while(true);
     }
     
 }
--- src/jdbcapp/dao/ProductDAO.java
+++ src/jdbcapp/dao/ProductDAO.java
@@ -2,6 +2,8 @@ package jdbcapp.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import jdbcapp.impl.ProductInterface;
 import jdbcapp.model.Product;
 import jdbcapp.service.GetConnection;
@@ -37,10 +39,84 @@ public class ProductDAO implements ProductInterface{
 
     @Override
     public int delete(Product p) {
+        Connection con = null;
+        int x = 0;
+        try{
+            con = GetConnection.getConnection();
+            String sql = "delete from product where id = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, p.getId());
+            x = ps.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                con.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return x;
     }
 
     @Override
     public int update(Product p) {
+      Connection con = null;
+      int x = 0;
+      try{
+         con = GetConnection.getConnection();
+         String sql = "update product set name=?,price=?,description=? where id=?";
+         PreparedStatement ps = con.prepareStatement(sql);
+         ps.setString(1, p.getName());
+         ps.setFloat(2, p.getPrice());
+         ps.setString(3, p.getDescription());
+         ps.setInt(4, p.getId());
+         x = ps.executeUpdate();
+      }
+      catch(Exception e){
+          e.printStackTrace();
+      }
+      finally{
+          try{
+              con.close();
+          }
+          catch(Exception e){
+              e.printStackTrace();
+          }
+      }
+      return x;
     }
-    
+    public ArrayList<Product> getProductList(){
+      Connection con = null;
+        ArrayList<Product> al  = new ArrayList<>();
+      try{
+          con = GetConnection.getConnection();
+          String sql = "select * from product";
+          PreparedStatement ps = con.prepareStatement(sql);
+          ResultSet rs = ps.executeQuery();
+          while(rs.next()){
+              int id = rs.getInt(1);
+              String name = rs.getString(2);
+              float price = rs.getFloat(3);
+              String desc =rs.getString(4);
+              Product p = new Product(id, name, price, desc);
+              al.add(p);
+          }
+      }
+      catch(Exception e){
+          e.printStackTrace();
+      }
+      finally{
+          try{
+              con.close();
+          }
+          catch(Exception e){
+              e.printStackTrace();
+          }
+      }
+      return al;
+    }
 }
